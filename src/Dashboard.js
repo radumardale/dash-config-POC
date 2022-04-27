d47b5974b438e7f2367e73d28a1ecb11520c459b
Radu Mardale <rmardale@riasolutionsgroup.com>
2022-04-27 13:18:40 +0300

    upgraded React

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import RGL, { WidthProvider } from 'react-grid-layout';
 import WidgetWrapper from './WidgetWrapper';
 // import silv from "./sylvester.png";
@@ -15,8 +15,8 @@ const getId = () => {
   return idCounter.toString();
 };
 
-class Dashboard extends React.PureComponent {
-  static defaultProps = {
+const Dashboard = () => {
+  const defaultProps = {
     isDraggable: true,
     isResizable: true,
     items: 15,
@@ -32,122 +32,102 @@ class Dashboard extends React.PureComponent {
     isDroppable: true,
   };
 
-  state = {
-    layout: [
-      { x: 0, y: 0, w: 1, h: 2, i: getId() },
-      { x: 0, y: 1, w: 3, h: 2, i: getId() },
-    ],
-    showGuideLines: false,
-  };
+  const [showGuideLines, setShowGuideLines] = React.useState(false);
+  const [layout, setLayout] = React.useState([
+    { x: 0, y: 0, w: 1, h: 2, i: '1' },
+    { x: 0, y: 1, w: 3, h: 2, i: '2' },
+  ]);
 
-  render() {
-    const rows =
-      this.state.layout.reduce((a, i) => {
-        return Math.max(a, i.y + i.h);
-      }, 1) * 2;
+  // state = {
+  //   layout: ,
+  //   showGuideLines: false,
+  // };
 
-    // console.log(this.state.layout);
-    return (
-      <React.Fragment>
-        <div className="page-wrapper">
-          <div className="left-panel">
-            <div className="left-top-section">
-              <span>Available widgets</span>
+  const rows =
+    layout.reduce((a, i) => {
+      return Math.max(a, i.y + i.h);
+    }, 1) * 2;
 
-              <div className="drop-removed-items">
-                <span>
-                  Drag items here to remove them from the summary layout.
-                </span>
-              </div>
-            </div>
+  // console.log(layout);
+  return (
+    <React.Fragment>
+      <div className="page-wrapper">
+        <div className="left-panel">
+          <div className="left-top-section">
+            <span>Available widgets</span>
 
-            <div>
-              <div
-                className="droppable-element"
-                draggable={true}
-                unselectable="on"
-                key="miau"
-                onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
-                data-grid={{ h: 2, w: 2, minW: 2 }}
-              >
-                Droppable Element (Drag me!)
-              </div>
+            <div className="drop-removed-items">
+              <span>
+                Drag items here to remove them from the summary layout.
+              </span>
             </div>
           </div>
 
-          <div className="right-panel">
-            <div className="dash-wraper">
-              {this.state.showGuideLines && (
-                <div className="grid-decoy-wrapper">
-                  {new Array(rows * 6)
-                    .fill(null)
-                    .map((_, index) => index)
-                    .map((v) => {
-                      return <div className="decoy"></div>;
-                    })}
-                </div>
-              )}
-              <ReactGridLayout
-                {...this.props}
-                isDroppable={true}
-                onLayoutChange={(layout) => {
-                  // console.log("onlayout change", e);
-                  // this.setState({ layout });
-                }}
-                onDrop={(layout, layoutItem, _event) => {
-                  // console.log(layoutItem);
-                  this.setState({ layout, showGuideLines: false });
-                }}
-                onDragStart={(e) => {
-                  this.setState({ showGuideLines: true });
-                }}
-                onResizeStart={(e) => {
-                  this.setState({ showGuideLines: true });
-                }}
-                onDragStop={(e) => {
-                  this.setState({ showGuideLines: false });
-                }}
-                onResizeStop={(e) => {
-                  this.setState({ showGuideLines: false });
-                }}
-              >
-                {this.state.layout.map((item) => (
-                  <WidgetWrapper
-                    style={{ borderRadius: 5 }}
-                    key={item.i}
-                    data-grid={item}
-                  >
-                    <span>{item.i}</span>
-                  </WidgetWrapper>
-                ))}
-              </ReactGridLayout>
+          <div>
+            <div
+              className="droppable-element"
+              draggable={true}
+              unselectable="on"
+              key="miau"
+              onDragStart={(e) => e.dataTransfer.setData('text/plain', '')}
+              data-grid={{ h: 2, w: 2, minW: 2 }}
+            >
+              Droppable Element (Drag me!)
             </div>
           </div>
         </div>
-      </React.Fragment>
-    );
-  }
 
-  addNewItem = () => {
-    const { layout } = this.state;
-    const newItem = { x: 0, y: 0, w: 3, h: 3, i: getId() };
-
-    if (layout.some((item) => item.x === 0 && item.y === 0)) {
-      this.setState({
-        layout: layout
-          .map((item) => {
-            if (item.x === 0) {
-              return { y: item.y++, ...item };
-            }
-
-            return item;
-          })
-          .concat([newItem]),
-      });
-    } else {
-      this.setState({ layout: layout.concat([newItem]) });
-    }
-  };
-}
+        <div className="right-panel">
+          <div className="dash-wraper">
+            {showGuideLines && (
+              <div className="grid-decoy-wrapper">
+                {new Array(rows * 6)
+                  .fill(null)
+                  .map((_, index) => index)
+                  .map((v) => {
+                    return <div className="decoy"></div>;
+                  })}
+              </div>
+            )}
+            <ReactGridLayout
+              {...defaultProps}
+              isDroppable={true}
+              onLayoutChange={(layout) => {
+                // console.log("onlayout change", e);
+                // this.setState({ layout });
+              }}
+              onDrop={(layout, layoutItem, _event) => {
+                setLayout(layout);
+                setShowGuideLines(false);
+              }}
+              onDragStart={(e) => {
+                setShowGuideLines(true);
+              }}
+              onResizeStart={(e) => {
+                setShowGuideLines(true);
+              }}
+              onDragStop={(e) => {
+                setShowGuideLines(false);
+              }}
+              onResizeStop={(e) => {
+                setShowGuideLines(false);
+              }}
+            >
+              {layout.map((item) => (
+                <WidgetWrapper
+                  style={{ borderRadius: 5 }}
+                  key={item.i}
+                  data-grid={item}
+                >
+                  <span>{item.i}</span>
+                </WidgetWrapper>
+              ))}
+            </ReactGridLayout>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import './reset.css';
 import './fontFace.css';
 import './styles.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const root = createRoot(rootElement);
+root.render(<App />);
